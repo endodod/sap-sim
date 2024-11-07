94d5b7e2d42c63c2acf3783e432ed6b318dec18e
endodo <anmelderhuhnx@mailbox.org>
2024-11-07 23:23:19 +0100

    first steps
    planned ability types, triggers and targeting as well as basic class for pets and foods

---
+++ main.py
@@ -0,0 +1,16 @@
+class Pet:
+    def __init__(self):
+        self.name = None
+        self.attack =  None
+        self.health = None
+        self.ability = None
+        self.lvl = 1
+        self.lvl_progress = 0
+        self.perk = None
+
+class Food:
+    def __init__(self):
+        self.name = None
+        self.ability = None
+        self.lvl = 1
+        
